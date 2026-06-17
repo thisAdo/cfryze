0816ffceb14622f182183f1f68bbf3b1f8f0f465
Ado <meadonix@gmail.com>
2026-06-16 20:55:44 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+import { execSync } from 'child_process';
+
+if (!process.env.CHROME_PATH) {
+  try {
+    const chromePath = execSync('which google-chrome-stable || which google-chrome').toString().trim();
+    if (chromePath) {
+      process.env.CHROME_PATH = chromePath;
+    }
+  } catch {}
+}
+
 import { ScraperPool } from './lib/scraper-pool.js';
 
 export { ScraperPool };
